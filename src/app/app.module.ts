357625ab05f73c7c42320956c187056fdfde0911
Lewis Matos <lewiismb1@example.com>
2021-05-12 11:05:34 +0200

    Creando los componentes!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewComponent } from './components/new/new.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
