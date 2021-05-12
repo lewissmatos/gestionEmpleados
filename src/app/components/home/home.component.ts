414b5a2754382ab97a4373f26f10ff204c7debfd
Lewis Matos <lewiismb1@example.com>
2021-05-12 18:12:59 +0200

    conervison de edades en la pantalla de lista de empleados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,19 +14,43 @@ export class HomeComponent implements OnInit {
     this.getAllEmpleados()
    }
 
+
+  empleados: Empleado[] = []
+
+  fecha = '02-07-2002'
+
+  edad = 0
+
+  edades:number[] = []
+  fechas = []
+  
   ngOnInit(): void {
   }
 
-  empleados:Empleado[] = []
 
   getAllEmpleados() {
     this.data.getAllEmpleado().subscribe(
       res => {
-        this.empleados  = res.data  
+        this.empleados = res.data
+        this.fechas = res.data.map((x: any) => {
+          this.edades.push(this.calcularEdad(x.fechaNac))
+        }) 
       }
     )
   }
 
+  calcularEdad(fecha: any) {
+    let hoy = new Date();
+    let cumpleanos = new Date(fecha);
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    let m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+      edad--;
+    }
+
+    return edad;
+}
 
   deleteEmpleado(_id: any) {
     Swal.fire({
