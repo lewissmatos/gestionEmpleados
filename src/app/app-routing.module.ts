459ec972f9a6e9b75232b03a21a8f11188e65e74
Lewis Matos <lewiismb1@example.com>
2021-05-12 11:30:49 +0200

    diseñando el home page y dandole estilo a las tablas con bootstrap y estilos propios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditComponent } from './components/edit/edit.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewComponent } from './components/new/new.component';
+import { ViewComponent } from './components/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'new', component: NewComponent},
+  {path: 'edit', component: EditComponent},
+  {path: 'view', component: ViewComponent },
+  {path:'**', pathMatch:'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
